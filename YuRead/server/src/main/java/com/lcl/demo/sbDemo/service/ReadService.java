7987e7dacc75984b6d26fc50e8b8383790f32485
Sally Piao <46841790+1752124@users.noreply.github.com>
2019-12-26 05:16:57 +0800

    Add files via upload

--- YuRead/server/src/main/java/com/lcl/demo/sbDemo/service/ReadService.java
+++ YuRead/server/src/main/java/com/lcl/demo/sbDemo/service/ReadService.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 public interface ReadService {
 
-	int insert(Long userId, Long bookId, Integer state);
+	int insert(Long userId, Long bookId, Integer state, String title, String author, String publisher);
 
 	int update(Long userId, Long bookId, Integer state);
 
 	List<Read> select(Long userId, Integer state);
 
-	Read getDataById(Long id);
+	Read getDataById(Long userId, Long bookId);
 
-	List<Read> selectByLikeTitle(String  keyword);
+	List<Read> selectByLikeTitle(Long userId, String  keyword);
 
 }
